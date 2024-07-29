909a56161f2fb2df0327b2fdc68019efc9bced95
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-07-29 13:43:18 +0530

    Update changepasswordback.php

--- VITmeds/changepasswordback.php
+++ VITmeds/changepasswordback.php
@@ -10,7 +10,7 @@ $opass=$_GET['oldPassword'];
 $npass=$_GET['newPassword'];
 $servername = "sql6.freesqldatabase.com";
 $username = "sql6701824";
-$pwd = "2WQAWP7vnq";
+$pwd = "";
 $dbname = "sql6701824";
 
 $conn = mysqli_connect($servername, $username, $pwd, $dbname);
