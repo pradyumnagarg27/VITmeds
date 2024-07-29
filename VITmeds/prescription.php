bd824dfadeb73a151433cae894d6641585cbc733
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-07-29 13:42:22 +0530

    Update prescription.php

--- VITmeds/prescription.php
+++ VITmeds/prescription.php
@@ -9,7 +9,7 @@ $reg = $_GET['reg'];
 $aid = $_GET['aid'];
 $servername = "sql6.freesqldatabase.com";
 $username = "sql6701824";
-$pwd = "2WQAWP7vnq";
+$pwd = "";
 $dbname = "sql6701824";
 
 $conn = mysqli_connect($servername, $username, $pwd, $dbname);
