4cdaac4c8d839245566d9afd036b37a4499f2f10
Pradyumna Garg <130541684+pradyumnagarg27@users.noreply.github.com>
2024-07-29 13:44:14 +0530

    Update studentprofile.php

--- VITmeds/studentprofile.php
+++ VITmeds/studentprofile.php
@@ -22,7 +22,7 @@ if($mode=="student"){
 }
 $servername = "sql6.freesqldatabase.com";
 $username = "sql6701824";
-$pwd = "2WQAWP7vnq";
+$pwd = "";
 $dbname = "sql6701824";
 
 $conn = mysqli_connect($servername, $username, $pwd, $dbname);
